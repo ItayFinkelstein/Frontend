973746ec57934f64aa627727dbbd2a6cb4cb6d9a
Gil <gilgil.abramovich@gmail.com>
2025-02-28 17:06:56 +0200

    Add like button, switch when clicked

--- src/PostCard.tsx
+++ src/PostCard.tsx
@@ -3,6 +3,10 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { CardActions, IconButton } from '@mui/material';
+import FavoriteSelectedIcon from '@mui/icons-material/Favorite';
+import FavoriteUnselectedIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
 
 export type Post = {
     title: string,
@@ -16,6 +20,7 @@ type PostCardProps = {
 }
 
 export default function PostCard(props: PostCardProps) {
+    const [isLiked, setIsLiked] = useState(false)
     return (
         <Card sx={{maxWidth: 445}}>
             <CardHeader
@@ -26,13 +31,18 @@ export default function PostCard(props: PostCardProps) {
                 component="img"
                 height="194"
                 image={props.post.image}
-                alt="Paella dish"
+                // alt={props.post.title}
             />
             <CardContent>
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                     {props.post.description}
                 </Typography>
             </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites" style={{outline: 'none'}} onClick={() => setIsLiked((curr) => !curr)}>
+                    {isLiked ? <FavoriteSelectedIcon style={{color: 'red'}}/> : <FavoriteUnselectedIcon />}
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
