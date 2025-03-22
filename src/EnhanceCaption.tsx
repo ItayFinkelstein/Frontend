37f1036eadcaf7a4393f40291a349ad46e3ccacd
Itay <itay.finkelstein@gmail.com>
2025-03-22 22:11:26 +0200

    handle

--- src/EnhanceCaption.tsx
+++ src/EnhanceCaption.tsx
@@ -19,7 +19,6 @@ const EnhanceCaption: React.FC<EnhanceCaptionProps> = ({
   const [showEnhancedCaption, setShowEnhancedCaption] = useState(false);
 
   const handleEnhanceCaption = async () => {
-    console.log("Enhancing caption" + currentDescription);
     const enhanced = await generateEnhancedCaption(currentDescription);
     setEnhancedCaption(enhanced);
     setShowEnhancedCaption(true);
